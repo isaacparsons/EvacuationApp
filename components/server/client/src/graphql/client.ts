0d7509ef2792eddb91709de5cd1c77c5a28e791c
isaacparsons <isaac.2962@gmail.com>
2022-12-05 15:02:56 -0700

    updates

--- components/server/client/src/graphql/client.ts
+++ components/server/client/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: '/',
+  uri: 'http://ec2-3-23-225-206.us-east-2.compute.amazonaws.com:4000',
   cache: new InMemoryCache()
 });
 
