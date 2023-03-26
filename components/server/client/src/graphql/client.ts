9ead5b1656236cc384218171bc13f527cadf7fa6
isaacparsons <isaac.2962@gmail.com>
2023-03-26 15:56:29 -0600

    updated website apollo uri

--- components/server/client/src/graphql/client.ts
+++ components/server/client/src/graphql/client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  // uri: 'http://52.42.98.231:4000/graphql',
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://52.42.98.231:4000/graphql',
+  // uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 });
 
